6b7325067731080ff246a6721cd0763f299ef72b
Tristan-BS <98057075+Tristan-BS@users.noreply.github.com>
2024-11-28 15:53:03 +0100

    Changelog 28.11.2024 - 15:52
    -> Fixed dumbest mistake ever and added break; between switch case xD
    -> The current position: Everything is working
    
    -> TODO:
         -> Typespeed
         -> Statistics
         -> Database
         -> Settings
    -> "FINISHED"
         -> TextList ENUM

--- src/main/java/CK/Typespeed.java
+++ src/main/java/CK/Typespeed.java
@@ -6,9 +6,10 @@ import org.apache.logging.log4j.Logger;
 
 public class Typespeed {
 
-    private static final Logger LOG = LogManager.getLogger(Main.class);
+    private final Logger LOG = LogManager.getLogger(Main.class);
     static Scanner GetInput = new Scanner(System.in);
     TextList TextList;
+    private String Text;
 
     public void StartTypeSpeedTest(int Seconds, int Difficulty) {
         // TODO: Create Typespeed Test
@@ -25,20 +26,43 @@ public class Typespeed {
             case 1: {
                 LOG.info("Getting Easy Text...");
                 TextList = CK.TextList.EASY;
+                break;
             }
             case 2: {
                 LOG.info("Getting Normal Text...");
                 TextList = CK.TextList.NORMAL;
+                break;
             }
             case 3: {
                 LOG.info("Getting Hard Text...");
                 TextList = CK.TextList.HARD;
+                break;
             }
             case 4: {
                 LOG.info("Getting Impossible Text...");
                 TextList = CK.TextList.IMPOSSIBLE;
+                break;
             }
-
+            default: {
+                throw new IllegalArgumentException("Unknown Difficulty: " + Difficulty);
+            }
         }
+        SetText(TextList.getText());
+        LOG.info(TextList.getText());
+    }
+
+
+
+    // SETTER
+
+    private void SetText(String TextToType) {
+        Text = TextToType;
+    }
+
+
+    // GETTER
+
+    private String GetText() {
+        return Text;
     }
 }
