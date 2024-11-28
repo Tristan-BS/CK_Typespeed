460bc5f6bbf815edce31e5e40b343f6a03d61239
Tristan-BS <98057075+Tristan-BS@users.noreply.github.com>
2024-11-28 15:03:51 +0100

    First Commit - Logging

---
+++ src/main/java/CK/Main.java
@@ -0,0 +1,12 @@
+package CK;
+import java.util.Scanner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    private static final Logger LOG = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        LOG.info("Typespeed Program started!");
+    }
+}
