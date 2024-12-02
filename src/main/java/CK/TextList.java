4424e43c18f174770599018aae7506e245bef895
Tristan-BS <98057075+Tristan-BS@users.noreply.github.com>
2024-12-02 08:18:46 +0100

    Your commit message

--- src/main/java/CK/TextList.java
+++ src/main/java/CK/TextList.java
@@ -1,10 +1,10 @@
 package CK;
 
 public enum TextList {
-    EASY("That is a simple easy Typing Speed Test."),
-    NORMAL("Thats a Normal Typespeed Test, house of Glory its great Yes."),
-    HARD("Very Hard right now beCause You Never Know WHats Coming nexT do you? Nah u dont know bruh."),
-    IMPOSSIBLE("HOw CaN YOu Do ThIS? ItS NOt POsIbLE WTf?!");
+    EASY("That is a simple easy Typing Speed Test. The quick brown fox jumps over the lazy dog."),
+    NORMAL("Thats a Normal Typespeed Test, house of Glory its great Yes. Programming is fun, especially when you are learning new things."),
+    HARD("Very Hard right now beCause You Never Know WHats Coming nexT do you? Nah u dont know bruh. The complexities of code can be overwhelming at times."),
+    IMPOSSIBLE("HOw CaN YOu Do ThIS? ItS NOt POsIbLE WTf?! Typing at this speed requires immense concentration and skill.");
 
     private final String Text;
 
